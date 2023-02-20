55aad22d71fc7a631b6a610c17064059261a6869
vynijales <vynijales@gmail.com>
2023-02-19 23:56:52 -0300

    This em Arrow Function

---
+++ node/thisArrowFunction.js
@@ -0,0 +1,19 @@
+// This em Arrow Function
+// Fala, galera. Tudo bom com vocês?
+
+// Bom, como vimos na aula anterior, o this em uma “função tradicional”
+// vai apontar para o objeto global.
+
+// Isso não é nenhuma novidade, pois já vimos isso na aula passada.
+// Porém, quando falamos de arrow funcions, a coisa muda um pouco.
+
+// Isso porque dentro de uma arrow function,o this não vai apontar mais
+// para o objeto global, e sim para o module.export / exports.
+
+
+// Isso pode causar um pouco de confusão, mas não é nada de outro mundo.
+// Com um pouco mais de estudo você entende perfeitamente esse funcionamento. Vamos ao exemplo?
+
+
+// É importante que nós tenhamos isso claro em nossa mente. Mas se ainda ficou um pouco confuso, não se preocupe.
+// Esse assunto ainda vai ser muito abordado no curso.

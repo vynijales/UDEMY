85f519cdfb4fbf0d8a88db0d0410f9f1273e8631
vynijales <vynijales@gmail.com>
2023-02-23 12:30:56 -0300

    Projeto: API com Express #03

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,7 +2,12 @@ const porta = 3003 // porta 80 é a porta padrão quando você faz uma requisiç
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+
+app.use(bodyParser.urlencoded({ extended: true })) // vai retornar uma função midlware
+
 
 app.get('/produtos', (req, res, next) => { // http://localhost:3003/produtos
     res.send(bancoDeDados.getProdutos())
@@ -16,11 +21,26 @@ app.get('/produtos/:id', (req, res, next) => { // http://localhost:3003/produtos
 
 app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
-    res.send(produto)
+    res.send(produto) // JSON
 })
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
+})
+
 
 app.listen(porta, () => {
     console.log(`Servidor executando na porta ${porta}.`)
@@ -54,3 +74,6 @@ app.listen(porta, () => {
 //     }
 //   }
   
+
+// npm i --save body-parser@1.18.2 -E
+

6fbf781782a93e3a863f4f6a22ddf6e755630f68
vynijales <vynijales@gmail.com>
2023-03-03 15:16:28 -0300

    Promise #05

---
+++ esnext/promise5.js
@@ -0,0 +1,25 @@
+const { reject } = require("lodash");
+
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        try {
+            con.log('temp')
+            if(Math.random() < chanceErro) {
+                reject('Ocorreu um erro!')
+            } else {
+                resolve(valor)
+            }
+        } catch(e) {
+            reject(e)
+        }
+    })
+}
+
+funcionarOuNao('Testando', 0.5)
+    .then(v => `Valor: ${v}`)
+    .then(v => con.log(v),
+        err => console.log(`Erro Esp.: ${err}`)
+    )
+    .then(() => console.log('Quase Fim!'))
+    .catch(err => console.log(`Erro Geral: ${err}`))
+    .then(() => console.log('Fim!'))

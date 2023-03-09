1490d6448583e78be511360ac30bc1016325d2ee
vynijales <vynijales@gmail.com>
2023-03-09 01:25:36 -0300

    Formulário #03

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -1,5 +1,7 @@
 // npm init -y // para criar o arquivo package.json
 // npm i --save express@4.16.3 body-parser@1.18.2 -E
+// npm i --save-dev nodemon
+// nodemon server.js
 
 const express = require('express') // importar o express
 const app = express() // instanciar o express e associar à essa variável
@@ -9,7 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true})) // urlencoded é o padrão, se
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns. Usuário Incluido!!!</h1> <br>' + JSON.stringify(req.body))
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alerado!!!</h1> <br>'+ JSON.stringify(req.body))
 })
 
 app.listen(5500)

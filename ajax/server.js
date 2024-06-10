bcc86a0380128b35f044b9ff01ed84d2f627481b
vynijales <vynijales@gmail.com>
2024-06-10 19:12:53 -0300

    Implementando server.js

---
+++ ajax/server.js
@@ -0,0 +1,10 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.')) // serve static files from the current directory
+app.use(bodyParser.urlencoded({ extended: true })) // parse URL-encoded bodies
+app.use(bodyParser.json()) // parse JSON bodies
+
+app.get('/teste', (req, res) => res.send('Hello, World!'))
+app.listen(8081, () => console.log('Server running on http://localhost:8081/'))

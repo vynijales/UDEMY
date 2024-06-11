5b0ef346c52df93df48f27a8c680ce06ca4add0c
vynijales <vynijales@gmail.com>
2024-06-11 09:47:29 -0300

    Fetch API #02

--- ajax/server.js
+++ ajax/server.js
@@ -29,4 +29,13 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+
+})
+
+
 app.listen(8081, () => console.log('Server running on http://localhost:8081/'))

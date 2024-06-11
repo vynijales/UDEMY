639b003050adfededb2e3ef4680bc269fdbb8ead
vynijales <vynijales@gmail.com>
2024-06-11 08:58:41 -0300

    XMLHttpRequest #02 (upload de arquivos)

--- ajax/server.js
+++ ajax/server.js
@@ -6,4 +6,27 @@ app.use(express.static('.')) // serve static files from the current directory
 app.use(bodyParser.urlencoded({ extended: true })) // parse URL-encoded bodies
 app.use(bodyParser.json()) // parse JSON bodies
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
 app.listen(8081, () => console.log('Server running on http://localhost:8081/'))
